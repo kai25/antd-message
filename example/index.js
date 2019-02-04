29cfb31a3041041adf9305222fafe9acc85ff400
vomelchenko <v.omelchenko@smartweb.com.ua>
2019-02-04 14:26:07 +0200

    make icons inline

--- example/index.js
+++ example/index.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { message } from '../src/message/index.js';
 
 import './style.less';
 
-class App extends Component {
-  static handleClick(type, content) {
-    message.show(type, content);
-  }
-  render() {
-    return (
-      <div className="app-wrap">
-        <div>
-          <button type="primary" onClick={() => App.handleClick('success', <div>132</div>)}>Yoo</button>
-        </div>
+function App() {
+  return (
+    <div className="app-wrap">
+      <div>
+        <button type="primary" onClick={() => message.success(<div>Success</div>)}>Success</button>
+        <button type="primary" onClick={() => message.error(<div>Error</div>)}>Error</button>
+        <button type="primary" onClick={() => message.warn(<div>Warn</div>)}>Warn</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
